1fd012f381f756bb38813fb403a38bdbaff1bc1d
Juan Carlos Perez <jcperezt@student.ie.edu>
2021-10-07 15:54:16 +0200

    add new function func2

--- Class061021.py
+++ Class061021.py
@@ -6,5 +6,11 @@ Created on Wed Oct  6 10:29:48 2021
 @author: cachopereztinoco
 """
 def func(a,b):
-    return a + b 
+    return a + b
+
+    def func2(a,b):
+        return a * b
+
+        
+        
 
